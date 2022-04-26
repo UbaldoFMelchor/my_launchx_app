29e62a07031d166fdcbe48493d649a162b35236d
UbaldoFMelchor <ubaldofmelchor97@hotmail.com>
2022-04-25 22:53:01 -0500

    corregido error ortografico test y test fail hecho

--- test/missionCommander.test.js
+++ test/missionCommander.test.js
@@ -3,5 +3,6 @@ const MissionCommander = require('../app/missionCommander');
 describe("Unit test for Mission Commander", () => {
     test('1) Create a mission commander objet', () => {
         const myMissionCommander = new MissionCommander("Woopa")
+        expect(myMissionCommander.name).toBe("Wooper");
     });
 })
