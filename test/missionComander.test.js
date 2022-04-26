57a42d83e82f72f2b98325cfc9b00b2ce104cf1d
UbaldoFMelchor <ubaldofmelchor97@hotmail.com>
2022-04-25 22:42:44 -0500

    corregido test

--- test/missionComander.test.js
+++ test/missionComander.test.js
@@ -1,5 +1,5 @@
 describe("esto es una suite de pruebas", () => {
-    Test('Caso de prueba 1', () => {
+    test('Caso de prueba 1', () => {
         const result = 1 + 2
         expect(result).toBe(10);
     });
