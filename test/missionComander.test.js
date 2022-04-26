672691ae09cc628d458e022055ef4ae57170f5ac
UbaldoFMelchor <ubaldofmelchor97@hotmail.com>
2022-04-25 22:51:03 -0500

    modificado test para mission commander

--- test/missionComander.test.js
+++ test/missionComander.test.js
@@ -1,8 +1,7 @@
 const MissionCommander = require('../app/missionCommander');
 
 describe("Unit test for Mission Commander", () => {
-    test('Caso de prueba 1', () => {
-        const result = 1 + 2
-        expect(result).toBe(3);
+    test('1) Create a mission commander objet', () => {
+        const myMissionCommander = new MissionCommander("Woopa")
     });
 })
